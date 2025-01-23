9c1da1e98df3e7eaa884c2502649885b1c290196
Sunny Wali <sunnywali777@gmail.com>
2025-01-23 21:36:27 +0545

    made some changes

---
+++ model/listing.js
@@ -0,0 +1,18 @@
+const mongoose=require("mongoose");
+
+const listingSchema=new mongoose.Schema({
+    title:{
+        type:String,
+        required:true
+    },
+    description:String,
+    image:{
+        set:(v)=>(v===""?{url:"https://images.unsplash.com/photo-1454388683759-ee76c15fee26?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"}:v),
+    },
+    price:Number,
+    location:String,
+    country:String
+});
+const Listing=mongoose.model("Listing",listingSchema);
+
+module.exports=Listing;
