e8c3ec6e8b6f63cbbc6bf05dbdb54f019b9b6fc3
Sunny Wali <sunnywali777@gmail.com>
2025-01-24 11:12:44 +0545

    made some changes

--- models/listing.js
+++ models/listing.js
@@ -7,6 +7,7 @@ const listingSchema=new mongoose.Schema({
     },
     description:String,
     image:{
+        type:Object,
         default:{
             url:"https://images.unsplash.com/photo-1454388683759-ee76c15fee26?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         },
