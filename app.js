8ef20f72004964c882a4c796ad7554e4f9d40d4b
Sunny Wali <sunnywali777@gmail.com>
2025-01-29 18:44:28 +0545

    made some changes

--- app.js
+++ app.js
@@ -3,9 +3,10 @@ const app=express();
 const mongoose=require("mongoose");
 const Listing = require("./models/listing");
 const path=require("path");
+const ejsMate=require("ejs-mate");
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"views"));
-
+app.engine("ejs",ejsMate);
 const main=async()=>{
     await mongoose.connect("mongodb://127.0.0.1:27017/practise");
 }
@@ -28,6 +29,10 @@ app.get("/listing",async(req,res)=>{
     res.render("listing/index",{allListing});
 });
 
+//New Route
+app.get("/listing/new",(req,res)=>{
+    res.render("layouts/new.ejs");
+})
 app.listen(8080,()=>{
     console.log("server is listening to the port no 8080");
 });
