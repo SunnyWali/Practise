5605bfe6e25116262f77b8c5b0dc3d4b1b240a05
SunnyWali <sunnywali777@gmail.com>
2025-03-20 20:04:52 +0530

    made some changes

--- app.js
+++ app.js
@@ -95,6 +95,11 @@ app.all("*",(req,res,next)=>{
     next(new ExpressError(400,"Page doesnot Exists"));
 });
 
+//Error handling middleware 
+app.use((err,req,res,next)=>{
+    let{status=500,message="Error Occured"}=err;
+    
+})
 app.listen(8080,()=>{
     console.log("server is listening to the port no 8080");
 });
